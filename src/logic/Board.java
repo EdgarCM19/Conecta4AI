469955f0740f122836c5bed9b50545f7f6fbdb54
Edgar Castro <edgarcm1908@gmail.com>
2020-10-29 20:18:57 -0600

    Arreglado problema de getColumnIndex en Board

--- src/logic/Board.java
+++ src/logic/Board.java
@@ -17,7 +17,7 @@ public class Board {
 	
 	private void initBoard() {
 		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board.length; j++) {
+			for (int j = 0; j < board[i].length; j++) {
 				board[i][j] = '-';
 			}
 		}
@@ -25,19 +25,19 @@ public class Board {
 
 
 
-	public boolean addPiece(int row, char name) {
+	public boolean addPiece(int column, char name) {
 		int index;
-		if((index = getColumnIndex(row)) != -1) {
-			board[row][index] = name;
+		if((index = getColumnIndex(column)) != -1) {
+			board[index][column] = name;
 			return true;
 		}
 		return false;
 	}
 	
-	private int getColumnIndex(int row) {
+	private int getColumnIndex(int column) {
 		int index = -1;
-		for (int i = board[row].length; i >= 0; i--) {
-			if(board[row][i] == '-')
+		for (int i = board.length - 1; i >= 0; i--) {
+			if(board[i][column] == '-')
 				return i;
 		}
 		return index;
@@ -45,7 +45,10 @@ public class Board {
 	
 	public void print() {
 		for (int i = 0; i < board.length; i++) {
-			
+			for (int j = 0; j < board[i].length; j++) {
+				System.out.print(board[i][j]);
+			}
+			System.out.println("");
 		}
 	}
 	
--- src/logic/MainGame.java
+++ src/logic/MainGame.java
@@ -9,6 +9,15 @@ import gui.SceneLoader;
 public class MainGame {
 
 	public static void main(String[] args) {
+		Board test = new Board();
+		test.addPiece(3, '1');
+		test.addPiece(2, '1');
+		test.addPiece(4, '1');
+		test.addPiece(3, '2');
+		test.addPiece(5, '2');
+		test.addPiece(4, '2');
+		test.print();
+		System.out.println("Eh");
 		GameLoader game = new GameLoader();
 		game.setup(new SceneLoader(), new Dimension(1080, 720), false);
 		game.start();
